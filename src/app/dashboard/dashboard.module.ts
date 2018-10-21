1fb54b2742021a9476290440abf2a21bf611e926
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-21 09:11:47 +0200

    implemented dashboard module

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{
+      path: '',
+      component: DashboardPageComponent
+    }
+    ])
+  ],
+  declarations: [
+    DashboardPageComponent,
+  ],
+  providers: [],
+})
+
+export class DashboardModule {
+}
