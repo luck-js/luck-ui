e47ca9f8ef50b551aebc7e6b9629f78569a7c10e
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-21 15:36:17 +0200

    generated hew-happening-page.component

--- src/app/happening/happening.module.ts
+++ src/app/happening/happening.module.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HappeningPageComponent } from './happening-page/happening-page.component';
+import { NewHappeningPageComponent } from './new-happening-page/new-happening-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([{
-      path: '',
-      component: HappeningPageComponent
-    }]),
+    RouterModule.forChild([
+      {
+        path: '',
+        component: NewHappeningPageComponent
+      },
+      {
+        path: ':id',
+        component: HappeningPageComponent,
+      }]),
   ],
   declarations: [
-    HappeningPageComponent
+    HappeningPageComponent,
+    NewHappeningPageComponent
   ],
   providers: []
 })
---
+++ src/app/happening/new-happening-page/new-happening-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lk-new-happening-page',
+  templateUrl: './new-happening-page.component.html',
+  styleUrls: ['./new-happening-page.component.scss']
+})
+
+export class NewHappeningPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
