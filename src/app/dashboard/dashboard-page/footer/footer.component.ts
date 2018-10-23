177c34bf341d18efaaa039f7eae700a6f0ce2158
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-23 09:14:45 +0200

    implemented navbar and footer structure

---
+++ src/app/dashboard/dashboard-page/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lk-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  date = new Date();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
-
 import { HomeModule } from '../core/home/home.module';
 import { HappeningModule } from '../happening/happening.module';
 import { ParticipationHappeningModule } from '../participation-happening/participation-happening.module';
 import { MatchMemberModule } from '../match-member/match-member.module';
+
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+
+import { NavbarComponent } from './dashboard-page/navbar/navbar.component';
+import { FooterComponent } from './dashboard-page/footer/footer.component';
 
 @NgModule({
   imports: [
@@ -39,6 +42,8 @@ import { MatchMemberModule } from '../match-member/match-member.module';
   ],
   declarations: [
     DashboardPageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   providers: [],
 })
