1cf4885771c20bcbea81e743c4eae24e2ec848a5
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-23 10:01:19 +0200

    style simple footer and navbar

--- src/app/dashboard/dashboard-page/footer/footer.component.ts
+++ src/app/dashboard/dashboard-page/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
   date = new Date();
 
