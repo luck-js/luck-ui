143e629979acf365fa6fdf59ed653b28afb4f5c9
Daniel Karski <daniel.karski5q@gmail.com>
2018-11-16 14:14:54 +0100

    generated modal for application

---
+++ src/app/core/components/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lk-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
