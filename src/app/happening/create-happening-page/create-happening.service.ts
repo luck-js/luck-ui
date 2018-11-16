de0973b9486a039bff1f6e6041eab8fede7ae884
Daniel Karski <daniel.karski5q@gmail.com>
2018-11-16 06:13:38 +0100

    breake create and view happening to another template

---
+++ src/app/happening/create-happening-page/create-happening.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CreatedHappening, Happening } from '../happening-page/happening.model';
+
+@Injectable()
+export class CreateHappeningService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public publishHappening(id: string, happening: Happening): Observable<CreatedHappening> {
+    return this.httpClient
+      .post<CreatedHappening>(`happening/generate-detailed-participant-list-information/${id}`, {happening})
+  }
+}
--- src/app/happening/happening-page/happening.service.ts
+++ src/app/happening/happening-page/happening.service.ts
@@ -2,23 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { CreatedHappening, Happening } from './happening.model';
+import { CreatedHappening } from './happening.model';
 
 @Injectable()
 export class HappeningService {
   public createdHappeningSubject: BehaviorSubject<CreatedHappening> = new BehaviorSubject(null);
 
   constructor(private httpClient: HttpClient) {
-  }
-
-  public generateDetailedParticipantListInformation(id: string, happening: Happening): Observable<CreatedHappening> {
-    return this.httpClient
-      .post<CreatedHappening>(`happening/generate-detailed-participant-list-information/${id}`, {happening})
-      .pipe(
-        tap((data) => this.createdHappeningSubject.next(data)),
-        tap((data) => console.log('HappeningService.generateDetailedParticipantListInformation -> CreatedHappening: ', data)
-        )
-      );
   }
 
   public getCreatedHappening(id: string): Observable<CreatedHappening> {
