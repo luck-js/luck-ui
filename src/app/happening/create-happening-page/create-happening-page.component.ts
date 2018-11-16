f3e3ca3158188e495f3feae72a53f75441dac207
Daniel Karski <daniel.karski5q@gmail.com>
2018-11-16 05:25:41 +0100

    generate create-happening component

---
+++ src/app/happening/create-happening-page/create-happening-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lk-create-happening-page',
+  templateUrl: './create-happening-page.component.html',
+  styleUrls: ['./create-happening-page.component.scss']
+})
+export class CreateHappeningPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
