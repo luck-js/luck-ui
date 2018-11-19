1f162e53598124b2dfb8317137071bf03a516a2e
Daniel Karski <daniel.karski5q@gmail.com>
2018-11-19 07:02:34 +0100

    refactored mechanism to updated height for main container

--- src/app/core/containers/app/app.component.ts
+++ src/app/core/containers/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
-import { Router, NavigationEnd} from '@angular/router';
-import { fromEvent, Subject } from 'rxjs';
-import { debounceTime, takeUntil } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'lk-app-root',
@@ -10,75 +9,37 @@ import { debounceTime, takeUntil } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit, OnDestroy {
   @HostBinding('style.height')
-  public styleHeight: any;
+  public styleHeight: string;
 
-  private oryginalHeight: number;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
-     if (event instanceof NavigationEnd) {
-       (<any>window).ga('set', 'page', event.urlAfterRedirects);
-       (<any>window).ga('send', 'pageview');
-     }
-   });
- }
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
 
   ngOnInit() {
-    this.oryginalHeight = this.getHeightNativeElement();
-    console.log('this.oryginalHeight: ', this.oryginalHeight);
-    fromEvent(window, 'resize').pipe(
-      takeUntil(this.ngUnsubscribe),
-      debounceTime(250)
-    )
-      .subscribe((event) => this.windowResizeSubscriber());
-
-    // fromEvent(window, 'orientationchange').pipe(
-    //   takeUntil(this.ngUnsubscribe)
-    // )
-    //   .subscribe((event) => this.windowOrientationChangeSubscriber());
-
+    this.styleHeight = this.getStaticHeightForAppContainer();
   }
 
   ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
-
-
   }
 
-  private windowResizeSubscriber() {
-    this.recalculateSizeForAllComponents();
-  }
-
-  private windowOrientationChangeSubscriber() {
-    this.recalculateSizeForAllComponents();
-  }
-
-  private recalculateSizeForAllComponents() {
-    const currentHeight = this.getHeightNativeElement();
-    const newHeight = getPercent(this.oryginalHeight, currentHeight);
-    this.setStyleHeight(newHeight);
-    console.log('currentHeight: ', currentHeight);
-    console.log('this.oryginalHeight : ', this.oryginalHeight);
-    console.log('newHeight: ', newHeight);
+  private getStaticHeightForAppContainer(): string {
+    return returnStyleHeight(this.getHeightNativeElement());
   }
 
   private getHeightNativeElement(): number {
     return Number(window.innerHeight);
   }
-
-  private setStyleHeight(height: number) {
-    this.styleHeight = `${height}%`;
-  }
-
-
 }
 
-export function getPercent(a: number, b: number): number {
-  return (b !== 0) ? mathRound(a * 100 / b, 2) : 0;
-}
-
-function mathRound(number, decimalPlaces): number {
-  return Number(Math.round(number * 100).toFixed(decimalPlaces)) / 100;
+function returnStyleHeight(height: number): string {
+  return `${height}px`;
 }
