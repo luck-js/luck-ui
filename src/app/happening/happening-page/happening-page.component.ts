73cd0ef8e34aee81da97d14023ded8571a0d236e
Karsoneker <38076798+Karsoneker@users.noreply.github.com>
2018-11-25 17:46:44 +0100

    add mechanism to hidden empty content

--- src/app/happening/happening-page/happening-page.component.ts
+++ src/app/happening/happening-page/happening-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, Inject, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { WINDOW } from '../../app.module';
-import { takeUntil, tap } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import { ParticipantUniqueLinkData, ParticipantUniqueLinkDataView } from './happening.model';
+import { ParticipantUniqueLinkData, ParticipantUniqueLinkDataView} from './happening.model';
 import { HappeningService } from './happening.service';
 import { AppStateService } from '../../core/app-state.service';
 
@@ -16,6 +16,7 @@ export class HappeningPageComponent implements OnInit, OnDestroy {
 
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
+  public isDescription = false;
   public name = '';
   public description = '';
   public participantsUniqueLink: ParticipantUniqueLinkDataView[] = [];
@@ -34,6 +35,8 @@ export class HappeningPageComponent implements OnInit, OnDestroy {
         this.participantsUniqueLink = created ? mapToViewModel(created.participantList) : [];
         this.name = created ? created.name : '';
         this.description = created ? created.description : '';
+
+        this.isDescription = this.checkIsDescriptionEmpty(this.name, this.description);
       });
   }
 
@@ -45,6 +48,11 @@ export class HappeningPageComponent implements OnInit, OnDestroy {
   private getHost(): string {
     return this.window.location.host;
   }
+
+  private checkIsDescriptionEmpty(name: string, description: string) : boolean{
+    return name !== '' && description !== ''
+  }
+
 
   public getValue(id: string): string {
     return `http://${this.getHost()}/#/participation/${id}`;
