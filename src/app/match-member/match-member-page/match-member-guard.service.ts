7f60d2b66cf28020bff2b97568b71bfebe9fd1cc
Daniel Karski <daniel.karski5q@gmail.com>
2018-11-26 07:19:15 +0100

    implemented mechanism for match-member.page

--- src/app/match-member/match-member-page/match-member-guard.service.ts
+++ src/app/match-member/match-member-page/match-member-guard.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, Router, ActivatedRouteSnapshot} from '@angular/router';
 import {Observable, of} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import {MatchMemberService} from './match-member.service';
 
 
