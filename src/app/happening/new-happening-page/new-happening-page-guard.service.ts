6bea78e341834053e9e99db72e1f1c3449372491
Daniel Karski <daniel.karski5q@gmail.com>
2019-10-26 13:43:55 +0200

    Set default happening name from href query params

--- src/app/happening/new-happening-page/new-happening-page-guard.service.ts
+++ src/app/happening/new-happening-page/new-happening-page-guard.service.ts
@@ -3,15 +3,21 @@ import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { NewHappeningPageService } from './new-happening-page.service';
+import {AppStateService} from '../../core/app-state.service';
 
 
 @Injectable()
 export class NewHappeningPageGuardService implements CanActivate {
   constructor(private router: Router,
+              private appStateService: AppStateService,
               private newHappeningPageService: NewHappeningPageService) {
   }
 
   private getParticipationHappeningData(): Observable<boolean> {
+    const url = new URL(window.location.href);
+    const name = url.searchParams.get('name');
+    this.appStateService.setNameHappening(name);
+
     return this.newHappeningPageService
       .createNewHappening().pipe(
         map(() => true),
